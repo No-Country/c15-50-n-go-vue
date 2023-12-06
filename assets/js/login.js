e2c815172e5fef90a5d54094bd64bc64c42277e9
Jadir Rojas <1895yair@gmail.com>
2023-12-06 18:48:16 -0500

    logica del logín

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,23 +1,81 @@
 
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    // Validación del campo de email
-    const email = document.getElementById('email');
-    const emailValue = email.value;
-    if (emailValue.trim() === '') {
-        event.preventDefault();
-        email.classList.add('error');
-    } else {
-        email.classList.remove('error');  // Elimina la clase de error si el campo es válido
+document.addEventListener('DOMContentLoaded', function () {
+    
+   
+    const email = {
+        email: '',
+        password: ''
     }
 
-    // Validación del campo de password
-    const password = document.getElementById('password');
-    const passwordValue = password.value;
+ // selecciónar elementos de la interfaz
+    const inputEmail = document.getElementById('email');
+    const inputPassword = document.getElementById('password');
+    const button = document.querySelector('#loginForm button[type="submit"]')
 
-    if (passwordValue.trim() === '') {
-        event.preventDefault();
-        password.classList.add('error');
-    } else {
-        password.classList.remove('error');  // Elimina la clase de error si el campo es válido
+    // Asignar eventos
+    inputEmail.addEventListener('blur', validar);
+    inputPassword.addEventListener('blur', validar);
+
+    function validar(e) {
+       if (e.target.value.trim() === '') {
+           mostrarAlerta(`El ${e.target.id} es obligatorio`, e.target.parentElement);
+           email[e.target.name] = '';
+           comprobarEmail();
+           return; 
+        } 
+        
+        if(e.target.id === 'email' && !validarEmail(e.target.value)){
+            mostrarAlerta('El email no es válido', e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        };
+
+        limpiarAlerta(e.target.parentElement);
+
+        // asignar valores
+
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+        
+        // Comprobar el objeto de email
+        comprobarEmail();
+
     }
+
+    function mostrarAlerta(mensaje, referencia) {
+
+        limpiarAlerta(referencia);
+
+        // generar alerta en HTML
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        error.classList.add('error');
+
+        //insertar el error en el formulario
+        referencia.appendChild(error);
+    
+    }
+
+    function limpiarAlerta(referencia){
+        const alerta = referencia.querySelector('.error');
+        if (alerta) {
+            alerta.remove();
+        }
+    }
+
+    function validarEmail(email) {
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
+        const resultado = regex.test(email);
+        return resultado;
+    }
+
+    function comprobarEmail() {
+        if(Object.values(email).includes('')){
+            button.disabled = true;
+        }else{
+            button.disabled = false;
+        };
+    }
+
 });
+
