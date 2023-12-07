01745035ec5379dfc015bca50b23afb132e3bb24
Jadir Rojas <1895yair@gmail.com>
2023-12-07 17:13:57 -0500

    se agrega validación de password

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,81 +1,75 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-    
-   
     const email = {
         email: '',
         password: ''
     }
 
- // selecciónar elementos de la interfaz
     const inputEmail = document.getElementById('email');
     const inputPassword = document.getElementById('password');
-    const button = document.querySelector('#loginForm button[type="submit"]')
+    const button = document.querySelector('#loginForm button[type="submit"]');
 
-    // Asignar eventos
     inputEmail.addEventListener('blur', validar);
     inputPassword.addEventListener('blur', validar);
 
     function validar(e) {
-       if (e.target.value.trim() === '') {
-           mostrarAlerta(`El ${e.target.id} es obligatorio`, e.target.parentElement);
-           email[e.target.name] = '';
-           comprobarEmail();
-           return; 
-        } 
-        
-        if(e.target.id === 'email' && !validarEmail(e.target.value)){
+        if (e.target.value.trim() === '') {
+            mostrarAlerta(`El ${e.target.id} es obligatorio`, e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        }
+
+        if (e.target.id === 'email' && !validarEmail(e.target.value)) {
             mostrarAlerta('El email no es válido', e.target.parentElement);
             email[e.target.name] = '';
             comprobarEmail();
             return;
-        };
+        }
+
+        if (e.target.id === 'password' && !validarPassword(e.target.value)) {
+            mostrarAlerta('La contraseña debe tener al menos 8 caracteres', e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        }
 
         limpiarAlerta(e.target.parentElement);
 
-        // asignar valores
-
         email[e.target.name] = e.target.value.trim().toLowerCase();
-        
-        // Comprobar el objeto de email
         comprobarEmail();
+    }
 
+    function validarEmail(email) {
+        const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        return regex.test(email);
+    }
+
+    function validarPassword(password) {
+        return password.length >= 8;
     }
 
     function mostrarAlerta(mensaje, referencia) {
-
         limpiarAlerta(referencia);
 
-        // generar alerta en HTML
         const error = document.createElement('P');
         error.textContent = mensaje;
         error.classList.add('error');
 
-        //insertar el error en el formulario
         referencia.appendChild(error);
-    
     }
 
-    function limpiarAlerta(referencia){
+    function limpiarAlerta(referencia) {
         const alerta = referencia.querySelector('.error');
         if (alerta) {
             alerta.remove();
         }
     }
 
-    function validarEmail(email) {
-        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
-        const resultado = regex.test(email);
-        return resultado;
-    }
-
     function comprobarEmail() {
-        if(Object.values(email).includes('')){
+        if (Object.values(email).includes('')) {
             button.disabled = true;
-        }else{
+        } else {
             button.disabled = false;
-        };
+        }
     }
-
 });
-
